c9b034f1e9d6db8e4d1f9bcf4ca10a20549a8f4d
sinsin <62082948+sintiasnn@users.noreply.github.com>
2022-11-04 16:24:23 +0800

    add functions

--- app/Http/Controllers/ChartJSController.php
+++ app/Http/Controllers/ChartJSController.php
@@ -13,7 +13,7 @@ class ChartJSController extends Controller
      *
      * @return response()
      */
-    public function index()
+    public function a1()
     {
         //SELECT tabel_a.ikp_a1, COUNT(tabel_a.ikp_a1) as jumlah FROM tabel_a GROUP BY tabel_a.ikp_a1;
         $a1 = DB::table('tabel_a')
@@ -23,6 +23,32 @@ class ChartJSController extends Controller
         
         $labels = ['Puas','Sangat Puas','Tidak Puas'];
         $values = array_column($a1, 'jumlah');
+              
+        return view('chartjs', compact('labels', 'values'));
+    }
+    public function a2()
+    {
+        //SELECT tabel_a.ikp_a1, COUNT(tabel_a.ikp_a1) as jumlah FROM tabel_a GROUP BY tabel_a.ikp_a1;
+        $a2 = DB::table('tabel_a')
+                ->selectRaw('count(ikp_a2) as jumlah')
+                ->groupBy('ikp_a2')
+                ->get()->toArray();
+        
+        $labels = ['Puas','Sangat Puas','Tidak Puas'];
+        $values = array_column($a2, 'jumlah');
+              
+        return view('chartjs', compact('labels', 'values'));
+    }
+    public function a4()
+    {
+        //SELECT tabel_a.ikp_a1, COUNT(tabel_a.ikp_a1) as jumlah FROM tabel_a GROUP BY tabel_a.ikp_a1;
+        $a4 = DB::table('tabel_a')
+                ->selectRaw('count(ikp_a4) as jumlah')
+                ->groupBy('ikp_a1')
+                ->get()->toArray();
+        
+        $labels = ['Puas','Sangat Puas','Tidak Puas'];
+        $values = array_column($a4, 'jumlah');
               
         return view('chartjs', compact('labels', 'values'));
     }
